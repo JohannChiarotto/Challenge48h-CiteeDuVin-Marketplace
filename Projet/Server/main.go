377512b493b35f149952dec77bda9cd5a3dacdab
cjohann <johann.chiarotto@ynov.com>
2025-03-25 10:06:35 +0100

    set up arbo

---
+++ Projet/Server/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	fc "marketplace/Fonctions"
+	"net/http"
+)
+
+func main() {
+	fc.SetHttp()
+
+	fmt.Println("Serveur démarré sur http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Erreur lors du démarrage du serveur:", err)
+	}
+}
